755b8d326c0cf7fb83e7dc25cdb1648677caee69
Tristan Dubé <tristan.dube@gmail.com>
2019-07-16 21:03:02 -0400

    Added some test behavior
    Started adding function structures.
    
    Added a licence file. Good ol' MIT.

---
+++ src/commands.rs
@@ -0,0 +1,91 @@
+use chrono::prelude::*;
+
+/**
+ * Data about misc data for a request.
+ */
+#[derive(Debug)]
+pub struct Commondata {
+
+    /**
+     * Static identifier of the message type
+     */
+    id: &'static str,
+
+    /**
+     * Date the request was made.
+     */
+    request_date: chrono::DateTime<UTC>
+}
+
+/**
+ * Data about a particular user of a request.
+ */
+#[derive(Debug)]
+pub struct Userdata {
+    /**
+     * Identifier for the player.
+     */
+    user_id: std::string::String
+}
+
+/**
+ * Create default structures that are usually included in every request types.
+ * 
+ * Returned as a tuple with a copy of each type of structs. Can be extended if
+ * more structs are created.
+ * 
+ * The structs are created with a bunch of default values. Normally each
+ * structures would be created separately with proper data.
+ */
+fn generate_default_structs() -> (Userdata, Commondata) {
+    let default_user = Userdata {
+        user_id: std::string::String::from("Default")
+    };
+
+    let default_misc = Commondata {
+        id: "DEF",
+        request_date: chrono::UTC::now()
+    };
+
+    return (default_user, default_misc);
+}
+
+// HELLO
+#[derive(Debug)]
+pub struct HelloCommand {
+    commondat: Commondata,
+    userdat: Userdata,
+    msg: std::string::String
+}
+
+impl HelloCommand {
+    pub fn new() -> HelloCommand {
+        let (user, mut misc) = generate_default_structs();
+        misc.id = "HELLO";
+
+        return HelloCommand {
+            commondat: misc,
+            userdat: user,
+            msg: std::string::String::from("TEST_MESSAGE")
+        }
+    }
+
+    pub fn from_client_message(data: &[u8]) -> Result<HelloCommand, String> {
+        let (user, mut misc) = generate_default_structs();
+        misc.id = "HELLO";
+
+        let msg_ident = get_string_from_msgdata(data)?;
+
+        let created = HelloCommand {
+            userdat: user,
+            commondat: misc,
+            msg: msg_ident
+        };
+
+        return Ok(created);
+    }
+}
+
+fn get_string_from_msgdata(slice: &[u8]) -> Result<String, String> {
+    std::string::String::from_utf8(slice.to_vec()).map_err(|err| err.to_string())
+}
--- src/main.rs
+++ src/main.rs
@@ -2,20 +2,30 @@ use std::net::*;
 use std::io::*;
 use std::string::*;
 
+mod commands;
+use commands::*;
+
 fn main() {
     start_client_polling();
 
-    std::thread::sleep(std::time::Duration::from_millis(10000));
+    std::thread::sleep(std::time::Duration::from_millis(5000));
 
     let listener = TcpListener::bind("127.0.0.1:5555").unwrap();
+
+    let x = HelloCommand::new();
 
     loop {
         match listener.accept() {
             Ok((sock, addr)) => {
                 std::thread::spawn(move || {
-                    println!("Server thread start");
+                    println!("[S] Server thread start");
+                    println!("[S] {:?} Connected", addr);
 
-                    println!("{:?} Connected", addr);
+                    // Default of 30 seconds to test. Maybe use 10s ?
+                    let default_timeout = std::time::Duration::from_secs(10);
+
+                    sock.set_read_timeout(Some(default_timeout))
+                        .expect("[S] Unable to set read timeout");
 
                     let mut reader = BufReader::new(sock);
 
@@ -23,17 +33,41 @@ fn main() {
                     let mut buf = vec![];
 
                     loop {
-                        let bytes_read = reader.read_until(b'|', &mut buf).expect("Cannot read line.");
-                        //let bytes_read = reader.read_line(&mut my_str).expect("Cannot read line.");
+                        let bytes_read_result = reader.read_until(b'|', &mut buf);
 
-                        if bytes_read > 0 {
-                            println!("Got : {:?}", String::from_utf8(buf.clone()).unwrap());
+                        match bytes_read_result {
+                            Ok(bytes_read) => {
+                                if bytes_read == 0
+                                {
+                                    println!("[S] Input finished ?");
 
-                            buf.clear();
+                                    break;
+                                }
+
+                                let msg_ident = &buf[0..4];
+                                println!("TEst = {:?}", msg_ident);
+                                let ident_string = String::from_utf8(msg_ident.to_vec()).expect("SomethingSomething");
+
+                                if ident_string == "HELO"
+                                {
+                                    let msg = commands::HelloCommand::from_client_message(&buf[4..]).unwrap();
+
+                                    println!("[S] Build message = {:?}", msg);
+                                }
+
+                                println!("[S] Got : {:?}", String::from_utf8(buf.clone()).unwrap());
+
+                                buf.clear();
+                            },
+                            Err(e) => {
+                                println!("[S] {:?}", e);
+
+                                break;
+                            }
                         }
                     }
 
-                    println!("Server thread terminated !");
+                    println!("[S] Server thread terminated !");
                 });
             },
             Err(_) => panic!()
@@ -47,7 +81,7 @@ fn start_client_polling() {
             let connect_result = client_connect();
 
             if connect_result.is_err()  {
-                println!("Connection failed");
+                println!("[C] Connection failed");
             }
 
             std::thread::sleep(std::time::Duration::from_millis(2000));
@@ -59,13 +93,13 @@ fn client_connect() -> std::io::Result<()> {
     //let mut conn = TcpStream::connect_timeout(&SocketAddr::from(([127, 0, 0, 1], 5555)), std::time::Duration::from_secs(10))?;
     let mut conn = TcpStream::connect("localhost:5555")?;
 
-    conn.write_all(b"Hello hello|")?;
+    conn.write_all(b"HELO_THIS-IS-CONTENT|")?;
     std::thread::sleep(std::time::Duration::from_millis(2000));
 
-    conn.write_all(b"Hello 2|")?;
+    conn.write_all(b"HELO_THIS-IS-ALSO-CONTENT|")?;
     std::thread::sleep(std::time::Duration::from_millis(2000));
 
-    conn.write_all(b"Hello 3|")?;
+    conn.write_all(b"HELO_LAST-CONTENT|")?;
     std::thread::sleep(std::time::Duration::from_millis(2000));
 
     return Ok(());
